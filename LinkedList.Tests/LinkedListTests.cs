729725e575ee9041a400b5f59d15a81f7e0fbaf1
NikPletnev <klophah@gmail.com>
2021-10-28 20:42:09 +0400

    Linked list update

--- LinkedList.Tests/LinkedListTests.cs
+++ LinkedList.Tests/LinkedListTests.cs
@@ -12,7 +12,8 @@ namespace LinkedListTests
 
         public LinkedList GetTestLinkedList(int key)
         {
-            int[] array = new int[] { 3, 5, 12, 15, 32};
+            int[] array = new int[] { 3, 5, 12, 15, 32 };
+
             LinkedList returnLinkedList = key switch
             {
                 1 => new LinkedList(),
@@ -45,8 +46,8 @@ namespace LinkedListTests
         }
 
         [TestCase(1, new int[0])]
-        [TestCase(2, new int[1] {5})]
-        [TestCase(3, new int[5] {3, 5, 12, 15, 32})]
+        [TestCase(2, new int[1] { 5 })]
+        [TestCase(3, new int[5] { 3, 5, 12, 15, 32 })]
 
         public void ToArrayTest(int key, int[] expected)
         {
@@ -58,8 +59,8 @@ namespace LinkedListTests
 
         }
 
-        [TestCase(1, new int[] { 6}, 6)]
-        [TestCase(2, new int[] { 11 ,5 }, 11)]
+        [TestCase(1, new int[] { 6 }, 6)]
+        [TestCase(2, new int[] { 11, 5 }, 11)]
         [TestCase(3, new int[] { 56, 3, 5, 12, 15, 32 }, 56)]
 
 
@@ -85,6 +86,171 @@ namespace LinkedListTests
             actual.AddFirst(listToAdd);
 
             Assert.AreEqual(expected, actual.ToArray());
+        }
+
+
+        [TestCase(1, 5, new int[] { 5 })]
+        [TestCase(2, 6, new int[] { 5, 6 })]
+        [TestCase(3, 9, new int[] { 3, 5, 12, 15, 32, 9 })]
+
+
+        public void AddLastTest(int key, int val, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+
+            actual.AddLast(val);
+
+            Assert.AreEqual(expected, actual.ToArray());
+        }
+
+        [TestCase(1, new int[] { 1, 2, 3 })]
+        [TestCase(2, new int[] { 5, 1, 2, 3 })]
+        [TestCase(3, new int[] { 3, 5, 12, 15, 32, 1, 2, 3 })]
+
+
+        public void AddLastTestList(int key, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+            LinkedList listToAdd = GetTestLinkedLustToAdd();
+
+            actual.AddLast(listToAdd);
+
+            Assert.AreEqual(expected, actual.ToArray());
+
+        }
+
+        [TestCase(2, 0, 6, new int[] { 6, 5 })]
+        [TestCase(3, 2, 67, new int[] { 3, 5, 67, 12, 15, 32 })]
+
+
+        public void AddAtTest(int key, int idx, int val, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+
+            actual.AddAt(idx, val);
+
+            Assert.AreEqual(expected, actual.ToArray());
+        }
+
+
+        [TestCase(2, 0, new int[] { 1, 2, 3, 5 })]
+        [TestCase(3, 2, new int[] { 3, 5, 1, 2, 3, 12, 15, 32 })]
+
+        public void AddAtTestList(int key, int idx, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+            LinkedList listToAdd = GetTestLinkedLustToAdd();
+
+            actual.AddAt(idx, listToAdd);
+
+            Assert.AreEqual(expected, actual.ToArray());
+
+        }
+
+        [TestCase(2, 0, 90, new int[] { 90 })]
+        [TestCase(3, 3, 42, new int[] { 3, 5, 12, 42, 32 })]
+
+
+        public void SetTest(int key, int idx, int val, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+
+            actual.Set(idx, val);
+
+            Assert.AreEqual(expected, actual.ToArray());
+        }
+
+        [TestCase(2, new int[0])]
+        [TestCase(3, new int[] { 5, 12, 15, 32 })]
+
+        public void RemoveFirstTest(int key, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+
+            actual.RemoveFirst();
+
+            Assert.AreEqual(expected, actual.ToArray());
+        }
+
+        [TestCase(2, new int[0])]
+        [TestCase(3, new int[] { 3, 5, 12, 15 })]
+        public void RemoveLastTest(int key, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+
+            actual.RemoveLast();
+
+            Assert.AreEqual(expected, actual.ToArray());
+
+        }
+
+
+        [TestCase(2, 0, new int[0])]
+        [TestCase(3, 3, new int[] { 3, 5, 12, 32 })]
+        [TestCase(3, 4, new int[] { 3, 5, 12, 15 })]
+
+        public void RemoveAtTest(int key, int idx, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+
+            actual.RemoveAt(idx);
+
+            Assert.AreEqual(expected, actual.ToArray());
+
+        }
+
+        [TestCase(2, 1, new int[0])]
+        [TestCase(3, 3, new int[] { 15, 32 })]
+        [TestCase(3, 5, new int[0])]
+
+        public void RemoveFirstMultipleTest(int key, int n, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+
+            actual.RemoveFirstMultiple(n);
+
+            Assert.AreEqual(expected, actual.ToArray());
+
+        }
+
+        [TestCase(2, 1, new int[0])]
+        [TestCase(3, 2, new int[] { 3, 5, 12 })]
+        [TestCase(3, 5, new int[0])]
+        public void RemoveLasttMultipleTest(int key, int n, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+
+            actual.RemoveLastMultiple(n);
+
+            Assert.AreEqual(expected, actual.ToArray());
+        }
+
+        [TestCase(2, 0, 1, new int[0])]
+        [TestCase(3, 2, 2, new int[] { 3, 5, 32 })]
+        [TestCase(3, 0, 5, new int[0])]
+
+        public void RemoveAtMultipleTest(int key, int idx, int n, int[] expected)
+        {
+            LinkedList actual = GetTestLinkedList(key);
+
+            actual.RemoveAtMultiple(idx, n);
+
+            Assert.AreEqual(expected, actual.ToArray());
+        }
+
+        [TestCase(2, 5, 0)]
+        [TestCase(2, 2, -1)]
+        [TestCase(3, 15, 3)]
+        [TestCase(3, 32, 4)]
+
+
+        public void RemoveFirstTestVal(int key, int val, int expected)
+        {
+            LinkedList actualList = GetTestLinkedList(key);
+
+            int actual = actualList.RemoveFirst(val);
+
+            Assert.AreEqual(expected, actual);
         }
 
     }
--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -37,7 +37,7 @@ namespace Lists
                 }
                 lenght++;
             }
-           
+
             return lenght;
         }
 
@@ -54,13 +54,13 @@ namespace Lists
                 }
             }
             return array;
-          
+
         }
 
         public void AddFirst(int val)
         {
             Node newHead = new Node(val, _head);
-            _head = newHead; 
+            _head = newHead;
         }
 
         public void AddFirst(LinkedList list)
@@ -74,6 +74,265 @@ namespace Lists
             _head = list.GetHead();
 
         }
+
+        public void AddLast(int val)
+        {
+            Node newLastNode = new Node(val, null);
+            if (_head == null)
+            {
+                _head = newLastNode;
+            }
+            else
+            {
+                Node current = GetHead();
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newLastNode;
+            }
+            
+        }
+
+
+        public void AddLast(LinkedList list)
+        {
+            if (_head == null)
+            {
+                _head = list.GetHead();
+            }
+            else
+            {
+                Node current = _head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = list.GetHead();
+            }
+        }
+
+        public void AddAt(int idx, int val)
+        {
+            if (idx+1 > GetLength() || idx < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (GetLength() == 1)
+            {
+                AddFirst(val);
+            }
+            else
+            {
+                int count = 0;
+                Node current = _head;
+                Node tmpNode = new Node();
+
+                while (count != idx - 1)
+                {
+                    current = current.Next;
+                    count++;
+                }
+
+                tmpNode = current.Next;
+                Node newNode = new Node(val, tmpNode);
+                current.Next = newNode;
+            }
+        }
+
+        public void AddAt(int idx, LinkedList list)
+        {
+            if (idx + 1 > GetLength() || idx < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (GetLength() == 1)
+            {
+                AddFirst(list);
+            }
+            else
+            {
+                int count = 0;
+                Node currentMain = _head;
+                Node current = list.GetHead();
+                Node tmpNode = new Node();
+
+                while (count != idx - 1)
+                {
+                    currentMain = currentMain.Next;
+                    count++;
+                }
+
+                tmpNode = currentMain.Next;
+                currentMain.Next = list.GetHead();
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = tmpNode;
+            }
+        }
+
+
+        public void Set(int idx, int val)
+        {
+
+            if (idx + 1 > GetLength() || idx < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Node current = _head;
+            int count = 0;
+            while (count != idx)
+            {
+                current = current.Next;
+                count++;
+            }
+
+            current.Value = val;
+           
+        }
+
+        public void RemoveFirst()
+        {
+            if (GetLength() == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            _head = _head.Next;
+        }
+
+        public void RemoveLast()
+        {
+            if (GetLength() == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (GetLength() == 1)
+            {
+                _head = null;
+            }
+            else
+            {
+                Node current = _head;
+                while (current.Next.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = null;
+            }
+           
+        }
+
+        public void RemoveAt(int idx)
+        {
+            if (idx + 1 > GetLength() || idx < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (GetLength() == 1)
+            {
+                RemoveFirst();
+            }
+            else
+            {
+                Node current = _head;
+                int count = 0;
+                while (count != idx - 1)
+                {
+                    count++;
+                    current = current.Next;
+                }
+                current.Next = current.Next.Next;
+            }
+        }
+
+
+        public void RemoveFirstMultiple(int n)
+        {
+            if (n  > GetLength() || n < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (GetLength() == 1 && n == 1)
+            {
+                RemoveFirst();
+            }
+            else
+            {
+                Node current = _head;
+                for (int i = 0; i < n; i++)
+                {
+                    current = current.Next;
+                }
+                _head = current;
+            }
+
+        }
+
+
+        public void RemoveLastMultiple(int n)
+        {
+            if (n > GetLength() || n < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (GetLength() ==  n )
+            {
+                _head = null;
+            }
+            else
+            {
+                int newLength = GetLength() - n;
+                Node current = _head;
+                for (int i = 0; i < newLength - 1; i++)
+                {
+                    current = current.Next;
+                }
+                current.Next = null;
+            }
+        }
+
+        public void RemoveAtMultiple(int idx, int n)
+        {
+            if (idx + 1 > GetLength() || idx < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (GetLength() == n)
+            {
+                _head = null;
+            }
+            else
+            {
+                Node current = _head;
+                int count = 0;
+                while (count != idx - 1)
+                {
+                    count++;
+                    current = current.Next;
+                }
+
+                Node newNext = current;
+                for (int i = 0; i <= n; i++)
+                {
+                    newNext = newNext.Next;
+                }
+                current.Next = newNext;
+            }
+        }
+
+        public int RemoveFirst(int val)
+        {
+          
+        }
+
+        public void RemoveAllTest(int val)
+        {
+
+        }
+
 
 
         private Node GetHead()
